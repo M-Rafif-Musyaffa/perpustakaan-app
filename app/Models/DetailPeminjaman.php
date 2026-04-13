eb8d6e97f50b7099fab4e7e0b23c988b5c4628ba
M Rafif Musyaffa <88952324+M-Rafif-Musyaffa@users.noreply.github.com>
2026-04-13 12:31:14 +0700

    Update DetailPeminjaman.php

--- app/Models/DetailPeminjaman.php
+++ app/Models/DetailPeminjaman.php
@@ -8,15 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class DetailPeminjaman extends Model
 {
     use HasFactory;
-
     protected $table = 'detail_peminjamans';
-
     protected $fillable = ['peminjaman_id', 'buku_id'];
-
-    /**
-     * UBAH DISINI: Tambahkan relasi ke model Buku.
-     * Setiap detail peminjaman memiliki satu buku.
-     */
     public function buku()
     {
         return $this->belongsTo(Buku::class);
